34c23a530353912068aa57a683c915b9f8b8fdd8
wangluyu <2602649277@qq.com>
2019-09-03 17:49:40 +0800

    xiugai

--- 01-introduction/02-helliword-web.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-const http = require('http')
-
-http.createServer((req,res)={
-  res.end('hello word!');
-}).listen(8080);
-
--- 01-introduction/02-helloword-web.js
+++ 01-introduction/02-helloword-web.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node 
 
+const http = require('http');
 
-
-console.log('hello word!');
+http.createServer((req,res)=>{
+  res.end('hello world!');
+}).listen(8080);
